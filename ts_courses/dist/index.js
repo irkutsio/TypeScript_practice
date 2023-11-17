bdd313d18024aaf428743d19fff5d5aeb0609f45
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-11-17 22:34:43 +0200

    типізація

--- ts_courses/dist/index.js
+++ ts_courses/dist/index.js
@@ -13,8 +13,50 @@ let empty = null;
 let quantity = undefined; //явна типізіція
 name = '5';
 let age = 12;
-const user = {
-    name: 'max',
-    age: 12,
+const test = 'type';
+const btn = 'large';
+var Sizes;
+(function (Sizes) {
+    Sizes["small"] = "small";
+    Sizes["medium"] = "medium";
+    Sizes["large"] = "large";
+})(Sizes || (Sizes = {}));
+const btn2 = Sizes.medium;
+function add(num, num2) {
+    return num + num2;
+} // 58
+function add2(num, num2) {
+    return `${num}${num2}`; // 256
+}
+console.log(add(2, 56));
+console.log(add2(2, 56));
+function add3(num, num2) {
+    console.log(num + num2); // якщо нічого не повертаємо
+}
+add3(2, 56);
+function greeting(user) {
+    console.log(`Hello ${user.name}`);
+}
+function userConstructor(name, age, hobby) {
+    return { name, age, hobby };
+}
+console.log(userConstructor('NIl', 34, 'tennis'));
+const Car = {
+    color: 'red',
+    price: 30000,
+    currency: 'USD',
+    start(word) {
+        console.log('Start!' + ' ' + word + ' ' + this.color);
+    },
+};
+Car.start('Hello');
+const user7 = {
+    name: 'Qwe',
+    age: 34,
+};
+const user8 = {
+    name: 'Opp',
+    age: 23,
+    role: 'admin',
 };
 //# sourceMappingURL=index.js.map
--- ts_courses/src/index.ts
+++ ts_courses/src/index.ts
@@ -34,9 +34,100 @@ let age = 12;
 
 // obj.age = true // помилка
 
-type User = { name: string; age: number };
+// type User = { name: string; age: number | string }; // оператор або |
 
-const user: User = {
-	name: 'max',
-	age: 12,
+// const user: User = {
+// 	name: 'max',
+// 	age: '12',
+// };
+
+type myType = 'type' | 'srt' | 'num';
+
+const test: myType = 'type';
+
+// any / unknown /enum
+
+type sizes = 'small' | 'medium' | 'large';
+
+const btn: sizes = 'large';
+
+enum Sizes {
+	small = 'small',
+	medium = 'medium',
+	large = 'large',
+}
+
+const btn2: Sizes = Sizes.medium;
+
+function add(num: number, num2: number): number {
+	return num + num2;
+} // 58
+
+function add2(num: number, num2: number): string {
+	return `${num}${num2}`; // 256
+}
+
+console.log(add(2, 56));
+console.log(add2(2, 56));
+
+function add3(num: number, num2: number): void {
+	console.log(num + num2); // якщо нічого не повертаємо
+}
+add3(2, 56);
+
+type User = {
+	name: string;
+};
+
+function greeting(user: User): void {
+	console.log(`Hello ${user.name}`);
+}
+
+type newUser = {
+	name: string;
+	age: number;
+	hobby: string;
+};
+
+function userConstructor(name: string, age: number, hobby: string): newUser {
+	return { name, age, hobby };
+}
+
+console.log(userConstructor('NIl', 34, 'tennis'));
+
+type Car = {
+	color: string;
+	price: number;
+	currency: string;
+	// start: (word: string) => {};
+};
+
+const Car = {
+	color: 'red',
+	price: 30000,
+	currency: 'USD',
+	start(word: string) {
+		console.log('Start!' + ' ' + word + ' ' + this.color);
+	},
+};
+
+Car.start('Hello');
+
+//////////////////////////////
+
+type User78 = {
+	name: string;
+	age: number;
+	role? : string
+};
+
+const user7: User78 = {
+	name: 'Qwe',
+	age: 34,
+};
+
+const user8: User78 = {
+	name: 'Opp',
+	age: 23,
+	role: 'admin',
 };
